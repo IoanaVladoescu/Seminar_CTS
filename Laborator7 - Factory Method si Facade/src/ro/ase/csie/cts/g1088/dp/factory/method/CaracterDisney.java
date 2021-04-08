ee4ed4f87d10591bea99fc1f8badc4077abad1bf
Ioana Vladoescu <ioana.vladoescu15@yahoo.com>
2021-04-08 12:37:03 +0300

    Factory Method

---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterDisney.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public class CaracterDisney extends CaracterJoc{
+	
+	public CaracterDisney(String nume) {
+		this.nume=
+	}
+
+}
---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/FactoryAbstractCaracter.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public abstract class FactoryAbstractCaracter {
+	
+	public CaracterJoc get
+
+	public CaracterJoc getCaracter(TipCaracter tip, String nume) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
