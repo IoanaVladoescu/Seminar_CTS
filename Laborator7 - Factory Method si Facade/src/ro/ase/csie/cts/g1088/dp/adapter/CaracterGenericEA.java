948ac49a525fc62ece105cd9db4092a34f17c6fb
Ioana Vladoescu <ioana.vladoescu15@yahoo.com>
2021-04-08 13:30:26 +0300

    Implementare Facade si Adapter Obiecte

---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/adapter/CaracterGenericEA.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.g1088.dp.adapter;
+
+public class CaracterGenericEA implements InterfatacCaracterEA {
+	
+	String name;
+	int level;
+	
+	@Override
+	
+	@Override
+	
+	}
+	
+	
+	
+
+}
---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/facade/ConexiuneServerJoc.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1088.dp.facade;
+
+public class ConexiuneServerJoc {
+	
+	System.out.println("");
+
+}
